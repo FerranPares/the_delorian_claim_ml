21479efff5f49b0139662b358ebeecd92ad1fe29
FerranPares <ferran.pares@bsc.es>
2023-10-29 15:21:46 +0100

    Add script to download pretrained model

---
+++ download_pretrained_model.sh
@@ -0,0 +1,4 @@
+
+mkdir -p model
+mkdir -p model/pretrained
+wget https://download.openmmlab.com/mmdetection/v2.0/dcn/mask_rcnn_r101_fpn_dconv_c3-c5_1x_coco/mask_rcnn_r101_fpn_dconv_c3-c5_1x_coco_20200216-a71f5bce.pth -P model/pretrained
