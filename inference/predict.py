94e7d3362e6a3f3c211ebc359cfcdef49e15d6f9
FerranPares <ferran.pares@bsc.es>
2023-10-30 19:40:04 +0100

    Include visualization

--- inference/predict.py
+++ inference/predict.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import numpy as np
 from pathlib import Path
 
@@ -25,6 +26,7 @@ class Prediction:
     label_name: str
     score: float
     bbox: list[int, int, int, int]
+    visualization: np.ndarray
     
     @classmethod
     def from_mmdet_prediction(cls, mmdet_prediction: dict):
@@ -39,18 +41,27 @@ class Prediction:
         if max(scores_list) != scores_list[0]:
             raise RuntimeError("Something went wrong: First object score is not the highest!")
         
-        class_id = int(pred['labels'][0])
+        class_id = pred['labels'][0]
         return cls(
             label_id=class_id,
             label_name=classes[class_id],
-            score=float(pred['scores'][0]),
-            bbox=pred['bboxes'][0]
+            score=pred['scores'][0],
+            bbox=pred['bboxes'][0],
+            visualization=mmdet_prediction['visualization'][0]
         )
 
 def get_prediction(np_img: np.ndarray) -> dict:
     # Perform inference
-    mmdet_prediction_dict = inferencer(np_img)
+    mmdet_prediction_dict = inferencer(np_img, return_vis=True)
     # Post-process and instantiates Prediction object
     return Prediction.from_mmdet_prediction(mmdet_prediction_dict)
 
 
+
+if __name__ == '__main__':
+    img_path = Path("data/CarDD_COCO/test2017/000040.jpg")
+    img = Image.open(img_path)
+    np_img = np.asarray(img)
+    pred = get_prediction(np_img)
+    Image.fromarray(pred.visualization).save(f"pred_{img_path.name}")
+    print(pred)
